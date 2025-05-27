8a961d86bc5bd18338ed70479040110de2da32ca
Khosuke <9814@holbertonstudents.com>
2025-05-27 09:19:16 +0200

    Added Task 2, 3 and 4: Extending the Teacher class. Printing teachers. And Writing a class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,15 +7,71 @@ interface Teacher {
     [key: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+
+const director1: Directors = {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    fullTimeEmployee: true,
+    location: "Hogwarts",
+    contract: true,
+    subject: "Magic",
+    house: "Gryffindor",
+    numberOfReports: 17,
+};
+console.log(director1);
 
 const teacher3: Teacher = {
-  firstName: "Albus",
-  lastName: "Dumbledore",
-  fullTimeEmployee: true,
-  location: "Hogwarts",
-  contract: true,
-  subject: "Magic",
-  house: "Gryffindor"
+    firstName: "Severus",
+    lastName: "Snape",
+    fullTimeEmployee: true,
+    location: "Hogwarts",
+    contract: false,
+    subject: "Potions",
+    headOfHouse: "Slytherin",
+    yearsOfExperience: 15
+};
+console.log(teacher3);
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return firstName.charAt(0) + ". " + lastName;
 };
 
-console.log(teacher3);
+interface Student {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass implements Student  {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+
+    workOnHomework() {
+        return ("Currently working")
+    }
+    displayName() {
+        return this.firstName
+    }
+}
+
+const hPotter = new StudentClass("Harry", "Potter");
+console.log(hPotter.displayName());
+console.log(hPotter.workOnHomework());
+
